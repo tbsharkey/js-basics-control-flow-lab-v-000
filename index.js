a5f930ee12b2825bd12abb4f7e72df537de0921e
Thomas Sharkey <tbsharkey@gmail.com>
2018-06-29 10:15:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ function scuberGreetingForFeet(someValue) {
   if (someValue <= 400) {
     greeting = 'This one is on me!'
   }
+  else if (someValue > 2000) {
+    greeting = 'I will gladly take your thirty bucks.'
+  }
+  else if (someValue > 2500) {
+    greeting = 'No can do.'
+  }
+  return greeting
 }
 
 function  ternaryCheckCity() {
