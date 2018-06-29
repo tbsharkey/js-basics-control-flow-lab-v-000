dbf89e17d4db6e05ccea5c6e7d95114a770c2f40
Thomas Sharkey <tbsharkey@gmail.com>
2018-06-29 10:53:03 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your code in this file!
+function scuberGreetingForFeet(someValue) {
+  let greeting
+  if (someValue <= 400) {
+    greeting = 'This one is on me!'
+  }
+  else if (someValue > 2000 && someValue < 2501) {
+    greeting = 'I will gladly take your thirty bucks.'
+  }
+  else  {
+    greeting = 'No can do.'
+  }
+  return greeting
+}
+
+function  ternaryCheckCity(city) {
+  let answer
+  (city === 'NYC' ? answer = 'Ok, sounds good.' : answer = 'No go.');
+  return answer
+}
+
+function switchOnCharmFromTip(tip) {
+  let response;
+  switch (tip) {
+    case 'generous':
+      response = 'Thank you so much.';
+      break;
+    case 'not as generous':
+      response = 'Thank you.';
+      break;
+    default:
+        response = 'Bye.';
+        break;
+  }
+  return response;
+}
