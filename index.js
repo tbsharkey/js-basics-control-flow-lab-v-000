3f83252f3bc78057436e5df7098efd3c8831dddb
Thomas Sharkey <tbsharkey@gmail.com>
2018-06-29 10:06:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Write your code in this file!
-function scuberGreetingForFeet(distance) {
-  if (distance <= 400) {
+function scuberGreetingForFeet(someValue) {
+  if (someValue <= 400) {
 
   }
 }
